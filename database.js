443e922e2d55e48fd3a9d8939ef62b54b1e9ce40
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-11-30 05:34:46 +0700

    update be

--- database.js
+++ database.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql")
 
+// const conn = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "orm-db",
+// })
+
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "orm-db",
+  host: "remotemysql.com",
+  user: "PeP1Bpdnlz",
+  password: "M3aLfKy2AY",
+  database: "PeP1Bpdnlz",
 })
 
 module.exports = conn
